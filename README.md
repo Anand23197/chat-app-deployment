bb144c510f299fb311c5482cda42bd499b7af307
Anand Vishwakarma <anand23197@gmail.com>
2024-03-08 08:08:39 +0530

    changes

---
+++ README.md
@@ -0,0 +1,6 @@
+# chat-app
+this is backend of chat app.
+# tech-stack
+Node, Mongo Db, Express, Socket.io.
+# Description 
+You can chat with any one. If you have not register register and login. you can also logout from chat. It is similar like chat 
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messagesRoutes = require("./routes/messagesRoutes");
+const path = require("path");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messagesRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("db connection successfull");
+  })
+  .catch((err) => {
+    console.log(`error in connection----> ${err}`);
+  });
+
+app.use(express.static(path.join(__dirname, "./chat-app-frontEnd/build")));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./chat-app-frontEnd/build/index.html"));
+});
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+  });
+});
